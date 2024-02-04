f0e3729703f6b161d732bfec9287091541382cf8
Raghu Anand <raghuaanand@gmail.com>
2024-02-04 19:47:54 +0530

    new release comp added

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
       <Navbar />
       <Header />
       {/* <Profile /> */}
-      <NewRelease />
       <DashboardStatus />
+      <div>
+       <NewRelease />
+      </div>
     </>
   )
 }
